dbceb0483f45fbb730c236eb19feaf0116825c5c
suelenadles <suelenadles@hotmail.com>
2023-05-16 14:34:04 -0300

    refatoring version final

--- src/controller/csvController.js
+++ src/controller/csvController.js
@@ -6,14 +6,10 @@ const csvController = {
   getParsedCsvData: async (req, res) => {
     try {
       const csvName = req.params.csvName;
-
       const csvPath = path.join('./data', `${csvName}.csv`);
-
-      const readerResult = await csvService.getReadCsvFile(csvPath);
-      const csvData = readerResult.csvData;
-      const columnNames = readerResult.columnNames;
-
-      res.status(200).json(csvTransformService.transformCsvData(columnNames, csvData));
+      const { csvData, columnNames } = await csvService.getReadCsvFile(csvPath);
+      const transformedData = csvTransformService.transformCsvData(columnNames, csvData);
+      res.status(200).json(transformedData);
     } catch (error) {
       res.status(400).send(error.message);
     }
--- src/service/csvTransformService.js
+++ src/service/csvTransformService.js
@@ -1,18 +1,18 @@
 const csvTransformService = {
-    transformCsvData: (columnNames, csvData) => {
-      const transformedData = csvData.map(
-        treatment => {
-        const obj = {};
-        for (let i = 0; i < columnNames.length; i++) {
-          let csvDataValue = 
-          treatment[i];
-        csvDataValue = csvDataValue.replace(/[^\w\s]/gi, '').replace(/_/gi, '').normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-          obj[columnNames[i]] = csvDataValue;
-        }
-        return obj;
-      });
-      return transformedData;
-    }
-  }
+  transformCsvData: (columnNames, csvData) => {
+  const transformedData = csvData.map(line => {
+    const obj = {};
+    columnNames.forEach((columnName, csvData) => {
+      obj[columnName] = line[csvData]
+          .replace(/[^\w\s]/gi, '')
+          .replace(/_/gi, '')
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '');
+    });
+    return obj;
+  });
+  return transformedData;
+}
+}
 
-  export default csvTransformService;
+export default csvTransformService;
